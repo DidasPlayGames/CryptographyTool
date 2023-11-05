54ddc150df48739e00934480bf3a34de5b934262
DidasPlayGames <didas161@gmail.com>
2023-11-05 21:18:29 +0000

    Added Fitness Calculations
    - Tetragram frequencies are now also calculated
    - The fitness of both monogram and tetragram frequencies is calculated
    - Corpus statistics have been moved into the script as variables rather than text files that are loaded in

--- Scripts/CiphertextAnalysis.py
+++ Scripts/CiphertextAnalysis.py
@@ -5,26 +5,30 @@ import numpy as np
 import pandas as pd
 from pandas import Series, DataFrame
 import matplotlib.pyplot as plt
+from numpy.linalg import norm
 
-# Establish a global dictionary for the integer equivalent of a character
+
+# Establish a global dictionary for the integer equivalent of a character, and frequency analysis
+# Monogram values are held as decimal, meanwhile tetragrams are held as a percentage
 conversionDictionary = {"a":1, "b":2, "c":3, "d":4, "e":5, "f":6, "g":7, "h":8, "i":9, "j":10, "k":11, "l":12, "m":13, "n":14, "o":15, "p":16, "q":17, "r":18, "s":19, "t":20, "u":21, "v":22, "w":23, "x":24, "y":25, "z":26}
+corpusMonograms = [0.0855, 0.0160, 0.0316, 0.0387, 0.1210, 0.0218, 0.0209, 0.0496, 0.0733, 0.0022, 0.0081, 0.0421, 0.0253, 0.0717, 0.0747, 0.0207, 0.0010, 0.0633, 0.0673, 0.0894, 0.0268, 0.0106, 0.0183, 0.0019, 0.0172, 0.0011]
+corpusTetragrams = {"tion" : 0.31, "nthe" : 0.27, "ther" : 0.24, "that" : 0.21, "ofth" : 0.19, "fthe" : 0.19, "thes" : 0.18, "with" : 0.18, "inth" : 0.17, "atio" : 0.17, "othe" : 0.16, "tthe" : 0.16, "dthe" : 0.15, "ingt" : 0.15, "ethe" : 0.15, "sand" : 0.14, "sthe" : 0.14, "here" : 0.13, "thec" : 0.13, "ment" : 0.12, "them" : 0.12, "rthe" : 0.12, "thep" : 0.11, "from" : 0.10, "this" : 0.10, "ting" : 0.10, "thei" : 0.10, "ngth" : 0.10, "ions" : 0.10, "andt" : 0.10}
+corpusMonogramMagnitude = np.linalg.norm(corpusMonograms)
+
 
 # Load in the ciphertext from the file
-ciphertext = str(open('InputAndOutputTexts\ciphertext.txt').read())
+ciphertext = str(open('InputAndOutputTexts\ciphertext.txt', encoding="utf-8").read())
 
-# Opens both corpus frequency files
-fCorpusMonograms = open("CorpusFrequencies\MonogramFrequencies.txt")
-fCorpusTetragrams = open("CorpusFrequencies\TetragramFrequencies.txt")
+# Formats the ciphertext so that only the text remains
+def FormatCiphertext():
+    global ciphertext
 
-# Creates a list and dictionary for corpus frequencies 
-corpusMonograms = [0] * 26
-corpusTetragrams = {"" : 0}
+    # Removes any spaces within the text, and converts it all into lowercase
+    ciphertext = ciphertext.replace(" ", "")
+    ciphertext = ciphertext.lower()
 
-# Loads in the monogram frequencies into a list
-for i in range(26):
-    corpusMonograms[i] = fCorpusMonograms.readline().replace("\n", "")
-
-print(corpusMonograms)
+    # Removes any special character from the text, and keeps only the text
+    ciphertext = re.sub('[\W\d_]+', '', ciphertext)
 
 # Converts a character into an integer depending on its alphabetical position
 def CharacterToInteger(character):
@@ -36,13 +40,6 @@ def IntegerToCharacter(integer):
 
 # When provided with a text, this function will return a list of monogram frequencies
 def CalculateMonogramFrequencies(inputText):
-    # Removes any spaces within the text, and converts it all into lowercase
-    inputText = inputText.replace(" ", "")
-    inputText = inputText.lower()
-
-    # Removes any special character from the text, and keeps only the text
-    inputText = re.sub('[\W\d_]+', '', inputText)
-
     # Splits the text into a list of individual characters
     textList = [*str(inputText)]
 
@@ -61,11 +58,66 @@ def CalculateMonogramFrequencies(inputText):
     # Returns the monogram frequencies to the program
     return frequencyCount
 
-frequencies = CalculateMonogramFrequencies(ciphertext)
+def CalculateTetragramFrequencies(inputText):
+    # Splits the text into a list of individual characters
+    textList = [*inputText]
+
+    # Creates an empty dictionary with all of the tetragram frequncies
+    frequencyCount = {"tion" : 0, "nthe" : 0, "ther" : 0, "that" : 0, "ofth" : 0, "fthe" : 0, "thes" : 0, "with" : 0, "inth" : 0, "atio" : 0, "othe" : 0, "tthe" : 0, "dthe" : 0, "ingt" : 0, "ethe" : 0, "sand" : 0, "sthe" : 0, "here" : 0, "thec" : 0, "ment" : 0, "them" : 0, "rthe" : 0, "thep" : 0, "from" : 0, "this" : 0, "ting" : 0, "thei" : 0, "ngth" : 0, "ions" : 0, "andt" : 0}
+
+    # Loops the tetragram detection for each possible tetragram in the text
+    for i in range(len(textList) - 3):
+        # Finds the tetragram at this index
+        tetragram = textList[i] + textList[i+1] + textList[i+2] + textList[i+3]
+
+        # Checks if it is a common tetragram, and if so adds it into the list
+        if str(tetragram) in frequencyCount:
+            frequencyCount[tetragram] += 1
+
+    # Places the frequencies into a list, so that they can be accessed by index
+    frequencyList = list(frequencyCount.values())
+
+    # Calculates the frequency of each tetragram into a percentage     
+    for i in range(30):
+        frequencyList[i] = (frequencyList[i] / (len(textList)-3)) * 100
+            
+    # Returns the frequencies as a list, with each index representing the order of the keys in the dictionary of corpusTetragrams
+    return frequencyList
+
+# Calculates the fitness of the given monogram frequencies
+# Value closer to 1 leads to better fitness
+def CalculateMonogramFitness(inputFrequencies):
+    global corpusMonograms
+    global corpusMonogramMagnitude
+
+    # Calculates the cosine between the 2 monogram frequencies, giving their fitness
+    fitnessValue = np.dot(inputFrequencies, corpusMonograms)/(norm(inputFrequencies)*corpusMonogramMagnitude)
+
+    # Returns the monogram fitness
+    return fitnessValue
+
+# Calculates the fitness of the given tetragram fitnesses
+# Lower negative leads to a better fitness
+def CalculateTetragramFitness(inputFrequencies):
+    global corpusTetragrams
+
+    corpusFrequencies = list(corpusTetragrams.values())
+
+    tetragramFitness = 0
+
+    for i in range(30):
+        tetragramFitness += inputFrequencies[i] * np.log(corpusFrequencies[i])
+
+    return tetragramFitness
+
+FormatCiphertext()
+
+monogramFrequencies = CalculateMonogramFrequencies(ciphertext)
+tetragramFrequencies = CalculateTetragramFrequencies(ciphertext)
+
+print(CalculateMonogramFitness(monogramFrequencies))
+print(CalculateTetragramFitness(tetragramFrequencies))
 
 
-plt.bar(conversionDictionary.keys(), frequencies)
-plt.show()
-
-
-
+# plt.bar(conversionDictionary.keys(), frequencies)
+# plt.show()
